8d65abe487cf5320a7e76558a067202e84e76552
Peter Burkholder <peter.burkholder@gsa.gov>
2016-10-31 21:21:10 -0400

    Cleaning up

--- src/inspec/V-38437.rb
+++
@@ -1,6 +0,0 @@
-# START_DESCRIBE V-38437
-  describe service('autofs') do
-    it { should_not be_enabled }
-    it { should_not be_running }
-  end
-# END_DESCRIBE V-38437
--- src/inspec/V-58901.rb
+++
@@ -1,5 +0,0 @@
-# START_DESCRIBE V-58901
-  # describe file('/etc') do
-  #   it { should be_directory }
-  # end
-# END_DESCRIBE V-58901
